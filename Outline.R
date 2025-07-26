f764b5228c4193f3a72da8ec6e54e5441aa90ae5
Emily Johnson <johnsonjemily@gmail.com>
2025-07-26 09:40:45 -0500

    7.26 9:40a dunn bros

---
+++ Outline.R
@@ -0,0 +1,14 @@
+Applied Psychometrics
+
+Why/how is this different:
+1. Organized based on project sequence
+2. Elements tied to standards 
+3. WebR for interactivity right there
+4. Graphics for different components**
+
+Ideas 
+creating technical reports, etc.
+Ben to potentially help with any graphics stuff.
+
+Resources to look at:
+Andy Fields
